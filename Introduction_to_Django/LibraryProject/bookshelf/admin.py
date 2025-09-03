f61ecbef089e7ae015850856a2b360b7d35270aa
MurisKamaa <engpaulkamau@gmail.com>
2025-09-03 21:33:11 +0300

    Added mods to admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns in list view
+    list_filter = ('publication_year', 'author')           # filters in right sidebar
+    search_fields = ('title', 'author')                    # search box
+
+# Register model with its custom admin class
+admin.site.register(Book, BookAdmin)
+
