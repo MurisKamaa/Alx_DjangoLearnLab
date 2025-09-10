053011510cd98cc29b55b5fffab4447940a74405
MurisKamaa <engpaulkamau@gmail.com>
2025-09-10 12:50:25 +0300

    Editted the views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.views.generic import DetailView
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship-app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
